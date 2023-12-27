4ebcc8301b8ab575015295b82d3c6f21f26cbf58
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-12-27 09:52:23 +0530

    first commit

--- index.js
+++ index.js
@@ -137,41 +137,41 @@ app.post("/reset-password/:token", async (req, res) => {
     }
 
 })
-app.post("/reset-password/:token", async (req, res) => {
-    try {
-        const { password, confirmPassword } = req.body
-        const token = req.params.token
-        jsonwebtoken.verify(token, secretKey,async (err, decoded) => {
-            try {
-                if (err) {
-                    res.json({
-                        message: "Error with token"
-                    })
-                } else {
-                    const hashedPassword = await bcrypt.hash(password, 10);
-                    const connection = await MongoClient.connect(URL)
-                    const db = connection.db("users")
-                    const user = await db.collection("Registered").findOne({ token: token })
+// app.post("/reset-password/:token", async (req, res) => {
+//     try {
+//         const { password, confirmPassword } = req.body
+//         const token = req.params.token
+//         jsonwebtoken.verify(token, secretKey,async (err, decoded) => {
+//             try {
+//                 if (err) {
+//                     res.json({
+//                         message: "Error with token"
+//                     })
+//                 } else {
+//                     const hashedPassword = await bcrypt.hash(password, 10);
+//                     const connection = await MongoClient.connect(URL)
+//                     const db = connection.db("users")
+//                     const user = await db.collection("Registered").findOne({ token: token })
     
-                    await db.collection("Registered").updateOne({ token }, {
-                        $set: {
-                            password: hashedPassword,
-                            confirmPassword: hashedPassword
-                        }
-                    })
-                    connection.close()
-                    res.send({ message: "Password changed succesfully", user })
-                } 
-            } catch (error) {
-                console.log(error)
-            }
-        })
+//                     await db.collection("Registered").updateOne({ token }, {
+//                         $set: {
+//                             password: hashedPassword,
+//                             confirmPassword: hashedPassword
+//                         }
+//                     })
+//                     connection.close()
+//                     res.send({ message: "Password changed succesfully", user })
+//                 } 
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         })
 
-    } catch (error) {
-        console.log(error)
-    }
+//     } catch (error) {
+//         console.log(error)
+//     }
 
-})
+// })
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT} ...`);
 })
